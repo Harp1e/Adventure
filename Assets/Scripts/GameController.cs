cdf366431ad19b4614f39ed7499243b969ad39af
Harp1e <github@bplayer.waitrose.com>
2017-04-05 16:10:15 +0100

    Configure room setup and add exit capability

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    [HideInInspector] public RoomNavigation roomNavigation;
+    [HideInInspector] public List<string> interactionDescriptionsInRoom = new List<string>();
+
+    public Text displayText;
+
+    List<string> actionLog = new List<string>();
+
+	void Awake ()
+    {
+        roomNavigation = GetComponent<RoomNavigation>();
+	}
+
+    void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+ 
+
+    public void DisplayLoggedText ()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray());
+        displayText.text = logAsText;
+    }
+	
+    public void DisplayRoomText ()
+    {
+        UnpackRoom();
+        string joinedInteractionDescriptions = string.Join("\n", interactionDescriptionsInRoom.ToArray());
+
+        string combinedText = roomNavigation.currentRoom.description + "\n" + joinedInteractionDescriptions;
+
+        LogStringWithReturn (combinedText);
+    }
+
+    void UnpackRoom ()
+    {
+        roomNavigation.UnpackExitsInRoom();
+    }
+
+    public void LogStringWithReturn (string stringToAdd)
+    {
+        actionLog.Add (stringToAdd + "\n");
+    }
+
+	void Update () {
+		
+	}
+}
